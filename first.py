f9322eb3d012711109efa99ca40e47fab080714b
zk <982847942@qq.com>
2023-03-17 19:55:38 +0800

    word2vec

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
-import  torch
+import torch
 import numpy as np
+
 # print(torch.__version__)
 # print(np.array([[1,2,3],[4,5,6]]))
 # print(torch.tensor(np.array([[1,2,3],[4,5,6]])))
@@ -8,6 +9,4 @@ import numpy as np
 # print(np.empty([3,4]))
 
 if __name__ == '__main__':
-    voca_dict = {'a':0,'b':1,'d':2,'c':3}
-    idx2word = {i:w for i,w in enumerate(voca_dict)}
-    print(idx2word)
+    t = 2;
